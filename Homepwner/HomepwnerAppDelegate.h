e369c5f6f5b65ad856cfda9daf23aca3a5f158a3
Alan Sparrow <trannguyenbaotrung@gmail.com>
2013-12-21 09:21:49 +0900

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by Alan Sparrow on 12/21/13.
+//  Copyright (c) 2013 Alan Sparrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Alan Sparrow on 12/21/13.
+//  Copyright (c) 2013 Alan Sparrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HomepwnerAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HomepwnerAppDelegate class]));
+    }
+}
